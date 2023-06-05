5b423cbea2a1e11d88c53c43a0618c04c7c929ad
EC2 Default User <ec2-user@ip-172-31-8-148.ap-northeast-1.compute.internal>
2023-06-05 01:10:29 +0000

    [Finish]4-9

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,9 +1,10 @@
 class PostImage < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-
-  has_one_attached :image
-
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_one_attached :image
+  belongs_to :user
+
 end
